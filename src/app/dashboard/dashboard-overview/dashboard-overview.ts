5245d73dcef9b8459b88256a1857679ff9b8786e
kaloa2025 <aalokchoudhari2021@gmail.com>
2025-11-03 05:20:42 +0530

    Update Dashboard Overview - Static Structure

--- src/app/dashboard/dashboard-overview/dashboard-overview.ts
+++ src/app/dashboard/dashboard-overview/dashboard-overview.ts
@@ -1,11 +1,20 @@
+import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { flush } from '@angular/core/testing';
 
 @Component({
   selector: 'app-dashboard-overview',
-  imports: [],
+  imports: [CommonModule, NgIf],
   templateUrl: './dashboard-overview.html',
   styleUrl: './dashboard-overview.css',
 })
 export class DashboardOverview {
+
+  showAddCategory = false;
+
+  toggleAddCategory()
+  {
+    this.showAddCategory=!this.showAddCategory;
+  }
 
 }
